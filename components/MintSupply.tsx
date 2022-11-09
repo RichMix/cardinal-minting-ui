397d4881bf632e9902547d960959fc737a935697
jpbogle <jpbogle22@gmail.com>
2022-11-09 14:46:29 -0500

    Remove supply

--- components/MintSupply.tsx
+++ components/MintSupply.tsx
@@ -1,11 +1,16 @@
 import { css } from '@emotion/react'
 import { useCandyMachineData } from 'hooks/useCandyMachineData'
+import { useProjectConfig } from 'providers/ProjectConfigProvider'
+import { useUTCNow } from 'providers/UTCNowProvider'
 
 export const MintSupply = () => {
   const candyMachineData = useCandyMachineData()
+  const { config } = useProjectConfig()
+  const { UTCNow } = useUTCNow()
   const pct =
     (parseInt(candyMachineData.data?.itemsRedeemed.toString() || '') ?? 0) /
     (parseInt(candyMachineData.data?.data.maxSupply.toString() || '') ?? 0)
+  if (config.goLiveSeconds && UTCNow < config.goLiveSeconds) return <></>
   return (
     <div className="relative h-8 w-full overflow-hidden rounded-xl bg-white bg-opacity-10">
       <div
